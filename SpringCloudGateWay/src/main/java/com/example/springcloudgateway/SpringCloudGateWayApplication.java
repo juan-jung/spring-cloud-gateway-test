c19ca20fbada5267d401faa49620d68dcc6d36bb
juan <crumbled23@gmail.com>
2024-07-09 15:01:18 +0900

    connect eureka server

--- SpringCloudGateWay/src/main/java/com/example/springcloudgateway/SpringCloudGateWayApplication.java
+++ SpringCloudGateWay/src/main/java/com/example/springcloudgateway/SpringCloudGateWayApplication.java
@@ -2,8 +2,10 @@ package com.example.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudGateWayApplication {
 
     public static void main(String[] args) {
