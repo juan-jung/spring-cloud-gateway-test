c2554bac450d7212b274f05eaf32421c2884bcd7
juan <crumbled23@gmail.com>
2024-07-11 13:19:28 +0900

    initial commit

---
+++ src/main/java/com/example/scgactuator/ScgActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.scgactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scgactuator/ScgActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scgactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
